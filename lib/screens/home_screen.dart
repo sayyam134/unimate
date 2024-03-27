2f34d4306c34a97a0a9b8ec7eaefaf468e424ef5
Aliasgarkuwakhedawala <aliasgarkuwakhedawala@gmail.com>
2024-03-27 12:17:09 +0530

    Added app bar and bottom navigation bar in home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,16 +9,79 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  var _currentindex = 0;
+
+  void _onSelectTab(index) {
+    setState(() {
+      _currentindex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            body: Center(
-      child: InkWell(
-          onTap: () {
-            logOut(context);
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(245, 237, 232, 1),
+          elevation: 0,
+          leading: const Icon(
+            Icons.notifications,
+            color: Colors.black,
+          ),
+          title: const Text(
+            'UniMate',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 28,
+              color: Colors.black,
+            ),
+          ),
+          actions: [
+            InkWell(
+              child: CircleAvatar(
+                child: Image.asset('assests/images/google_logo_color.png'),
+                radius: 15,
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            )
+          ],
+        ),
+        body: Center(
+          child: InkWell(
+              onTap: () {
+                logOut(context);
+              },
+              child: const Text("Go Back")),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            _onSelectTab(index);
           },
-          child: Text("Go Back")),
-    )));
+          currentIndex: _currentindex,
+          selectedFontSize: 16,
+          selectedIconTheme: const IconThemeData(
+            size: 28,
+          ),
+          backgroundColor: const Color.fromRGBO(138, 94, 65, 1),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white54,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_books_rounded),
+              label: 'NOTES',
+              backgroundColor: Color.fromRGBO(138, 94, 65, 1),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.groups_outlined),
+              label: 'COMMUNITY',
+              backgroundColor: Color.fromRGBO(161, 125, 100, 1),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
